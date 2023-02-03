14c26f085cc8119bb5d23a10fda74c5db0f0c0da
Lapo0 <lapo.gini.01@gmail.com>
2023-02-03 15:54:31 +0100

    FIX includes with for

--- js/app.js
+++ js/app.js
@@ -3,10 +3,20 @@
 const email = ['utente1@esempio.com', 'utente2@esempio.com', 'utente3@esempio.com']
 console.log(email)
 
+
 // Chiedere mail all'utente con un prompt, controllando che ci sia nella lista
 const emailUtente = prompt('Scrivi la tua mail')
 console.log(emailUtente)
 
+let mailEsistente = false
+for (let i = 0; i < email.length; i++) {
+  if (email[i] === emailUtente) {
+    mailEsistente = true
+    break
+  }
+}
+
+/*
 if (email.includes(emailUtente)) {
     console.log('La mail inserita è già presente nel database.')
 } else {
@@ -14,7 +24,16 @@ email.push(emailUtente)
 console.log(email)
 console.log('La sua mail: "' + emailUtente + '" è stata inserita nel database.' )
 }
+*/
 
+
+if (mailEsistente) {
+  console.log('La mail inserita è già presente nel database.')
+} else {
+  email.push(emailUtente)
+  console.log(email)
+  console.log('La sua mail: "' + emailUtente + '" è stata inserita nel database.' )
+}
 
 // GIOCO DEI DADI
 // Creare un numero randomico da 1 a 6 per giocatore e per il computer
