a81ecf0f48998a63c3536c67ac2bb64f16ddd43b
Lapo0 <lapo.gini.01@gmail.com>
2023-02-03 14:24:26 +0100

    FEAT email utente

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 console.log('ciao')
 
 // Creare array con email
+const email = ['utente1@esempio.com', 'utente2@esempio.com', 'utente3@esempio.com']
+console.log(email)
 
 // Chiedere mail all'utente con un prompt, controllando che ci sia nella lista
+const emailUtente = prompt('Scrivi la tua mail')
+console.log(emailUtente)
+
+email.push(emailUtente)
+console.log(email)
 
 // Stampa un messaggio con esito controllo
 
