c41dd6c95a017c5447089c75963eb2b6e7820935
Lapo0 <lapo.gini.01@gmail.com>
2023-02-03 14:39:59 +0100

    FEAT numero-casuale per utente e computer

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 console.log('ciao')
-
+// EMAIL UTENTE
 // Creare array con email
 const email = ['utente1@esempio.com', 'utente2@esempio.com', 'utente3@esempio.com']
 console.log(email)
@@ -10,7 +10,16 @@ console.log(emailUtente)
 
 email.push(emailUtente)
 console.log(email)
-console.log('La sua mail: "' + emailUtente + '" è stata inserita nel database.' )
 
 // Stampa un messaggio con esito controllo
+console.log('La sua mail: "' + emailUtente + '" è stata inserita nel database.' )
 
+// GIOCO DEI DADI
+// Creare un numero randomico da 1 a 6 per giocatore e per il computer
+const numeroRandomUtente = Math.floor(Math.random() * 6 + 1)
+console.log(numeroRandomUtente)
+
+const numeroRandomComputer = Math.floor(Math.random() * 6 + 1)
+console.log(numeroRandomComputer)
+
+// Stabilire il vincitore in base al numero più alto
