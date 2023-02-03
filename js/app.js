345ab5ef4dd972cfe8f5c3f75af99c177bff9ed3
Lapo0 <lapo.gini.01@gmail.com>
2023-02-03 16:03:49 +0100

    FEAT for cycle in dado-game

--- js/app.js
+++ js/app.js
@@ -9,10 +9,10 @@ const emailUtente = prompt('Scrivi la tua mail')
 console.log(emailUtente)
 
 let mailEsistente = false
+
 for (let i = 0; i < email.length; i++) {
   if (email[i] === emailUtente) {
     mailEsistente = true
-    break
   }
 }
 
@@ -26,7 +26,6 @@ console.log('La sua mail: "' + emailUtente + '" è stata inserita nel database.'
 }
 */
 
-
 if (mailEsistente) {
   console.log('La mail inserita è già presente nel database.')
 } else {
@@ -36,19 +35,25 @@ if (mailEsistente) {
 }
 
 // GIOCO DEI DADI
-// Creare un numero randomico da 1 a 6 per giocatore e per il computer
-const numeroRandomUtente = Math.floor(Math.random() * 6 + 1)
-console.log('Il tuo numero è: ' + numeroRandomUtente)
+// Giocare 10 partite
+const numeroPartite = parseInt( prompt('Quante partite vuoi giocare a Dadi?'))
 
-const numeroRandomComputer = Math.floor(Math.random() * 6 + 1)
-console.log('Il numero estratto dal computer è: ' + numeroRandomComputer)
+for (let i = 0; i < numeroPartite; i++) {
 
-// Stabilire il vincitore in base al numero più alto
+    // Creare un numero randomico da 1 a 6 per giocatore e per il computer
+    const numeroRandomUtente = Math.floor(Math.random() * 6 + 1)
+    console.log('Il tuo numero è: ' + numeroRandomUtente)
 
-if (numeroRandomUtente > numeroRandomComputer) {
-    console.log('Hai vinto :)')
-} else if (numeroRandomComputer > numeroRandomUtente){
-    console.log('Ha vinto il computer :(')
-} else if (numeroRandomComputer === numeroRandomUtente) {
-    console.log('Wow c\'è stato un pareggio!!')
+    const numeroRandomComputer = Math.floor(Math.random() * 6 + 1)
+    console.log('Il numero estratto dal computer è: ' + numeroRandomComputer)
+
+    // Stabilire il vincitore in base al numero più alto
+
+    if (numeroRandomUtente > numeroRandomComputer) {
+        console.log('Hai vinto :)')
+    } else if (numeroRandomComputer > numeroRandomUtente){
+        console.log('Ha vinto il computer :(')
+    } else if (numeroRandomComputer === numeroRandomUtente) {
+        console.log('Wow c\'è stato un pareggio!!')
+    }
 }
